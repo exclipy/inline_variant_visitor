7082340e5541c6595643941c2e8700c11eb80134
Kevin Wu Won <exclipy@gmail.com>
2012-03-15 23:53:32 +1100

    Use lambda expressions in the test

--- inline_variant_test.cpp
+++ inline_variant_test.cpp
@@ -3,25 +3,17 @@
 
 typedef boost::variant<int, char> IntChar;
 
-struct Func1
-{
-	int operator()(int x) const {
-        std::cout << "int " << x << std::endl;
-        return 1;
-	}
-};
-
-struct Func2
-{
-	int operator()(char x) const {
-        std::cout << "char " << x << std::endl;
-        return 2;
-	}
-};
-
 int main() {
     IntChar v('a');
     //v = 3;
-    int ret = boost::apply_visitor(make_visitor<int>(Func1(), Func2()), v);
+    int ret = boost::apply_visitor(make_visitor<int>(
+        [](int x) {
+            std::cout << "int " << x << std::endl;
+            return 1;
+        },
+        [](char x) {
+            std::cout << "char " << x << std::endl;
+            return 2;
+        }), v);
     std::cout << ret << std::endl;
 }
